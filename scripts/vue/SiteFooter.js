d4aaac074a0f622c36e9ce0caf8d5372db769bb3
DevelopLogan <148078754+developlogan@users.noreply.github.com>
2023-10-16 11:22:53 -0500

    SiteFooter changed to be Up-To-Date
    Updated links, added more people to the team section.

--- scripts/vue/SiteFooter.js
+++ scripts/vue/SiteFooter.js
@@ -2,16 +2,21 @@ export default {
   template: `
     <footer>
       <section>
-        <h2>The Team</h2>
-        <a href="//twitter.com/DevelopLogan">DevelopLogan - Founder</a>
-        <p>Manny204553 - Programmer</p>
-        <p>SeriousGuy888 - Web Dev</p>
+        <h2>Meet the Team</h2>
+        <a href="//twitter.com/DevelopLogan">DevelopLogan</a>
+        <a href="//twitter.com/Manny204553">Manny204553</a>
+        <a href="//twitter.com/SeriousGuy888">SeriousGuy888</a>
+        <a href="//twitter.com/JimmyPockets3">ItsNathanity</a>
+        <a href="//twitter.com/notvoltex">NotVoltex</a>
+        <a href="//youtube.com/@Mr3DGaming">Mr 3D</a>
+        <a href="//youtube.com/@raichuuX">raichuuX</a>
+        <a href="//youtube.com/@qwertyyfox">qwertyy</a>
       </section>
       <section>
         <h2>Links</h2>
-        <a href="//youtube.com/channel/UC_D3LnfeFVnC8AiB14kBVpg">YouTube</a>
+        <a href="//youtube.com/channel/UC_D3LnfeFVnC8AiB14kBVpg">YouTube Channel</a>
         <a href="//www.roblox.com/groups/33261894/rcreation-dev#!/about">Roblox Group</a>
-        <a href="//discord.gg/nmFk4e9zWg">Discord</a>
+        <a href="https://discord.gg/chFDZsjTH9">Discord Server</a>
       </section>
     </footer>
   `,
